5e8eb8833de7793f55440c48bedf72ea39d2f2f8
LTE1916 <2252351619@qq.com>
2023-09-26 18:39:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <LoginScene />
+  <SustechMap />
+</template>
+
+<script>
+import SustechMap from './components/SustechMap.vue'
+import LoginScene from './components/LoginScene.vue'
+export default {
+  components: {
+    LoginScene,
+    SustechMap
+  }
+}
+</script>
+
+<style>
+
+</style>
